7677f90583512096804201a990b50961e2c25ede
JaviCM <capillaj.dev@gmail.com>
2022-10-16 12:39:19 +0200

    Final version

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Market
+# Iniciacion a JavaScript con drag & drop, recreando un frontal de productos.
+**Proyecto**
+- Crear 2 cajas entendidas 1 como mostrador y otra como un carro de la compra, crear un total en el que sume el valor de todos los productos que se arrastren al carro.
+
+**Lenguajes usados**
+- HTML5
+- CSS3
+- JS
+
+**Agradecimientos**
+- Dar las gracias al equipo de <a href="https://geekshubsacademy.com/">GeeksHubs Academy</a> por los conocimientos adquiridos.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
 var vcarrito = document.getElementById("carrito")
+var vproductos = document.getElementById("productos")
 var productos = document.getElementsByClassName("producto")
+var vtotalcompra = document.getElementById("shoppingCartTotal")
+
+vcarrito.addEventListener("dragover", (ev) => permitirSoltar(ev))
+vcarrito.addEventListener("drop", (ev) => soltar(ev))
+vproductos.addEventListener("dragover", (ev) => permitirSoltar(ev))
+vproductos.addEventListener("drop", (ev) => soltar1(ev))
+
 for(var i=0; i<productos.length; i++){
     productos[i].setAttribute("draggable","true")
+    productos[i].addEventListener("dragstart",(ev) => iniciarArrastre(ev))
+}
+
+function iniciarArrastre(ev){
+    ev.dataTransfer.setData("idproducto",ev.target.id)
+}
+
+function permitirSoltar(ev){
+    ev.preventDefault()
+}
+
+function soltar(ev){
+    ev.preventDefault()
+    var data = ev.dataTransfer.getData("idproducto")
+    vcarrito.appendChild(document.getElementById(data))
+    calcular()
+}
+
+function soltar1(ev){
+    ev.preventDefault()
+    var data = ev.dataTransfer.getData("idproducto")
+    vproductos.appendChild(document.getElementById(data))
+    calcular()
+}
+
+function calcular(){
+    var children = vcarrito.children;
+    var price = 0;
+  for(var i=0; i<children.length; i++){
+            price = parseInt(children[i].dataset.price) + price;
+}
+vtotalcompra.innerText = price;
 }
